25d167a0dcfbea8d9ddb7918dc626b8d5ad2e031
ALina Gaiduk <gaidukalina18@yandex.ru>
2024-09-22 18:09:51 +0300

    реализованы юниттесты для сервиса кинотеатра

--- tests/lab4/task_one/test_cinema.py
+++ tests/lab4/task_one/test_cinema.py
@@ -1,15 +1,36 @@
+
+from  src.lab4.task_one.cinema import Recommendation
 import unittest
-from  src.lab4.task_one.cinema import User, Recommendation
-class TestUser(unittest.TestCase):
-        def test_same_views(self):
-                user1 =  User([1, 2, 3])
-                user2 = User([1, 3, 4])
-                user3 = User([2, 5, 7])
-                user4 = User([4, 8, 10, 3, 5])
-                self.assertEqual(user4.same_views(user2), 2/5)
-                self.assertEqual(user1.same_views(user3), 1/3)
-                self.assertEqual(user1.same_views(user2), 2/3)
-class TestRecommendation(unittest.TestCase):
-        def
+
+
+class TestRecommended(unittest.TestCase):
+
+    def test_recommend(self):
+        list_of_films = "films.txt"
+        history_file = "history.txt"
+        recommendation_system = Recommendation(list_of_films, history_file)
+        input_data = [2, 4]
+        expected_result = 'Дюна'
+        actual_result = recommendation_system.recommend(input_data)
+        self.assertEqual(actual_result, expected_result)
+
+
+    def test_history_list(self):
+        list_of_films = "films.txt"
+        history_file = "history.txt"
+        recommendation_service = Recommendation(list_of_films, history_file)
+        expected_history_sets = [
+            {2, 1, 3},
+            {1, 4, 3},
+            {2, 2, 2, 2, 2, 3}
+        ]
+
+        result = recommendation_service.history_list(history_file)
+        for i, user in enumerate(result[:3]):
+            self.assertEqual(user.watched_films, expected_history_sets[i])
+
+
+if __name__ == '__main__':
+        unittest.main()
 
 
